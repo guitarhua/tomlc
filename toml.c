d4c3ee657d85e66a81d7446542a1663fb4ea4bdc
kaan <kaan803@gmail.com>
2021-06-14 00:43:46 +0300

    implement toml_free and file reading code

--- toml.c
+++ toml.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 
@@ -7,7 +8,7 @@
  * be directly freed
  */
 struct toml_val {
-	char *key; /* UTF-8 string */
+	char *key; /* UTF-8 string, NULL if is an array element */
 
 	enum TOML_TYPE { /* TOML_NULL used only during parsing */
 		TOML_NULL, TOML_TABLE, TOML_ARRAY, TOML_INT, TOML_FLOAT,
@@ -31,65 +32,93 @@ struct toml_val {
 			int hour, min, sec, usec;
 		} time;
 		struct {
-			struct char **keys; /* pointers to vals[i].key */
-			struct toml_val *vals; /* list of subelts for a table */
-			size_t len;
+			struct char **keys; /* pointers to vals[i].key, NULL if empty table*/
+			struct toml_val **vals; /* list of subelts for table, NULL if empty table */
+			size_t len; /* length of keys and vals, 0 if empty table */
 		} table;
 		struct {
-			struct toml_val *vals; /* list of subelts for an array */
-			size_t len;
+			struct toml_val **vals; /* list of subelts for array, NULL if empty array */
+			size_t len; /* length of vals, 0 if empty table */
 		} array;
 	} u;
 };
 
-/*
- * parses and returns a TOML representation as a struct toml_val from a FILE*
- * returns NULL and writes to err[err_len] on failure
- * allocates memory using falloc and ffree if they are non-NULL
- * defaults to malloc(3) and free(3) otherwise
- */
-struct toml_val *toml_parse_file(FILE *fp, char *err, size_t err_len,
-		void *(*falloc)(size_t size), void (*free)(void *ptr));
 
-/*
- * parses and returns a TOML representation as a struct toml_val from a char[]
- * returns NULL and writes to err[err_len] on failure
- * allocates memory using falloc and ffree if they are non-NULL
- * defaults to malloc(3) and free(3) otherwise
- */
-struct toml_val *toml_parse_file(char *str, size_t len, char *err,
-		size_t err_len, void *(*falloc)(size_t size),
-		void (*free)(void *ptr));
+static char file_error_str[] = "error in reading from file stream";
+static size_t f_err_str_len = sizeof(file_error_str)/sizeof(file_error_str[0]);
+static char alloc_error_str[] = "error in memory allocation";
+static size_t a_err_str_len = sizeof(alloc_error_str)/sizeof(alloc_error_str[0]);
+
+
+struct toml_val *
+toml_parse_file(FILE *fp, char *err, size_t err_len,
+		void *(*falloc)(size_t size), void (*ffree)(void *ptr))
+{
+	size_t flen;
+	char *str;
+
+	if ((fseek(fp, 0, SEEK_END) < 0) || ((flen = ftell(fp)) < 0) || (fseek(fp, 0, SEEK_SET) < 0)) {
+		strncpy(err, file_error_str, err_len < f_err_str_len ? err_len : f_err_str_len);
+		return NULL;
+	}
+	if (!falloc) {
+		falloc = &malloc;
+	}
+	if (!ffree) {
+		ffree = &free;
+	}
+
+	if ((str = falloc(flen + 1)) == NULL) {
+		strncpy(err, alloc_error_str, err_len < a_err_str_len ? err_len : a_err_str_len);
+		return NULL;
+	}
+	if (fread(str, 1, flen, fp) < flen) {
+		ffree(str);
+		strncpy(err, file_error_str, err_len < f_err_str_len ? err_len : f_err_str_len);
+		return NULL;
+	}
+	str[flen] = '\0';
+	
+	return toml_parse_file(str, flen + 1, err, err_len, falloc, ffree);
+}
+
+
+struct toml_val *
+toml_parse_file(char *str, size_t len, char *err, size_t err_len,
+		void *(*falloc)(size_t size), void (*ffree)(void *ptr))
+{
+
+}
 
 
 void free_toml_val(struct toml_val *v, void (*ffree)(void *ptr))
 {
+	size_t i;
+
 	if (!v)
 		return;
 	if (!ffree)
 		ffree = &free;
 	switch (v->type) {
-	case TOML_NULL:
-	case TOML_INT:
-	case TOML_FLOAT:
-	case TOML_BOOL:
-	case TOML_DATE:
-	case TOML_TIME:
-		break;
 	case TOML_STRING:
-
-
+		(*ffree)(v->u.string);
+		break;
+	case TOML_DATETIME:
+		(*ffree)(v->u.datetime.z);
+		break;
+	case TOML_ARRAY:
+		for (i = 0; i < v->u.array.len; i++)
+			free_toml_val(v->u.array.vals[i], ffree);
+		(*ffree)(v->u.array.vals);
+		break;
+	case TOML_TABLE:
+		for (i = 0; i < v->u.table.len; i++)
+			free_toml_val(v->u.table.vals[i], ffree);
+		(*ffree)(v->u.table.vals);
+		(*ffree)(v->u.table.keys); /* individual keys are owned by subvals and freed with them */
 	}
-
-
-
-
-
-
-
-
-	free(v->key);
-	free(v);
+	(*ffree)(v->key);
+	(*ffree)(v);
 }
 
 
